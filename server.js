e9b7e03c4a01708ee65f7983a93e3840fbfe0573
chiijie155-ops <chiijie155@gmail.com>
2026-06-10 04:02:26 +0000

    dssdds

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+npm startnpm startnpm startrequire('dotenv').config();
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
